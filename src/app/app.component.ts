8a880b84afa245e2882361b490fee598c7733494
sia <violetGekkon@gmail.com>
2024-09-18 10:17:07 +0300

    logout added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {EcLayoutComponent} from "@easy-craft/layout";
+import {EcLayoutComponent} from '@easy-craft/layout';
+import {AuthService} from '@easy-craft/auth';
 
 @Component({
-  selector: 'ec-root',
-  standalone: true,
-  imports: [RouterOutlet, EcLayoutComponent],
-  template: `
-    <ec-layout></ec-layout>
-  `,
-  styles: [],
+    selector: 'ec-root',
+    standalone: true,
+    imports: [RouterOutlet, EcLayoutComponent],
+    template: `
+        <ec-layout (logout)="logout()"></ec-layout>
+    `,
+    styles: [],
 })
 export class AppComponent {
+    constructor(private _authService: AuthService) {
+    }
 
+    logout() {
+        this._authService.logout().subscribe();
+    }
 }
