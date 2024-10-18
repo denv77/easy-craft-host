bb4199bc42aef0e323aa63676c6a93e4a3963758
sia <violetGekkon@gmail.com>
2024-10-18 19:23:01 +0300

    ui-core lib added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {EcLayoutComponent} from '@easy-craft/layout';
 import {AuthService} from '@easy-craft/auth';
+import {MainLayoutComponent} from '@easy-craft/ui-core';
+import {manifestService, MfConfig} from './utils/manifest-service';
 
 @Component({
     selector: 'ec-root',
     standalone: true,
-    imports: [RouterOutlet, EcLayoutComponent],
+    imports: [RouterOutlet, MainLayoutComponent],
     template: `
-        <ec-layout (logout)="logout()"></ec-layout>
+        <ec-main-layout [menu]="menu" (onLogoutClick)="logout()"></ec-main-layout>
     `,
     styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    menu: any[] = [];
+
     constructor(private _authService: AuthService) {
+    }
+
+    ngOnInit() {
+        const manifest = manifestService.getManifest();
+
+        if (manifest) {
+            this.menu = manifest.map((mf: MfConfig) => ({
+                    title: mf.displayName,
+                    link: mf.routePath,
+                    icon: mf.remoteName === 'dashboard' ? 'home-outline' : 'person-outline',
+                    home: mf.remoteName === 'dashboard'
+                }
+            ));
+        } else {
+            console.error('Manifest is not loaded');
+        }
     }
 
     logout() {
         this._authService.logout().subscribe();
     }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,30 @@
 import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
-import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideRouter, Router} from '@angular/router';
-import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
-import {authInterceptor, AuthService, initializeUser} from "@easy-craft/auth";
-import {buildDynamicRoutes} from "./utils/build-dynamic-routes";
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {authInterceptor, AuthService, initializeUser} from '@easy-craft/auth';
+import {buildDynamicRoutes} from './utils/build-dynamic-routes';
+import {provideUiCore} from '@easy-craft/ui-core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter([]),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    ),
-    provideAnimationsAsync(),
-    AuthService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: buildDynamicRoutes,
-      deps: [Router],
-      multi: true,
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeUser,
-      multi: true
-    }
-  ]
+    providers: [
+        provideRouter([]),
+        provideHttpClient(
+            withInterceptors([authInterceptor])
+        ),
+        provideUiCore([]),
+        provideAnimationsAsync(),
+        AuthService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: buildDynamicRoutes,
+            deps: [Router],
+            multi: true,
+        },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeUser,
+            multi: true
+        }
+    ]
 };
