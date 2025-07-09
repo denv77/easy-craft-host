680b75716053ca2009f72eeccf0c4bbd2c0537b5
den <denis.vorobjew@yandex.ru>
2025-07-09 21:41:30 +0300

    fix bffUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  // bffUrl: 'https://docker.smartcom.msk.ru:8043',
+  bffUrl: 'https://docker.smartcom.msk.ru:8043',
   // bffUrl: 'http://127.0.0.1:8080',
-  bffUrl: 'http://localhost:8080',
+  // bffUrl: 'http://localhost:8080',
   maxCookieExpiresTime: 5 * 60, // За сколько секунд до окончания сессии появится диалоговое окно с предупреждением о завершении сессии
   minCookieExpiresTime: 2 * 60, // За сколько секунд до окончания сессии произойдет автоматический logout
   maxUserIdleTime: 10 * 60, // Сколько секунд пользователь может ничего не делать перед автоматическим logout
